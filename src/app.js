20af882bf6d3bbb959fa8eb34ad62f20e9b0e6ea
shivammajhi8991 <shivammajhi934@gmailcom>
2024-02-06 13:13:17 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,13 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes
+
+import userRouter from "./routes/user.route.js";
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
