d8f3ca1f1d2d49ff82b34aabc5ad94ffe92363c2
shivammajhi8991 <shivammajhi934@gmailcom>
2024-02-07 13:16:58 +0530

    fix all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { cloudinary } from "../utils/cloudinary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -15,7 +15,7 @@ const registerUser = asyncHandler(async (req, res) => {
   // return res
 
   const { fullName, email, username, password } = req.body;
-  console.log(password, email);
+  // console.log(password, email);
 
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -23,7 +23,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -32,14 +32,24 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocaLPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocaLPath) {
     throw new ApiError(400, "Avatar file is required");
   }
 
-  const avatar = await cloudinary(avatarLocaLPath);
-  const coverImage = await cloudinary(coverImageLocalPath);
+  const avatar = await uploadOnCloudinary(avatarLocaLPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
     throw new ApiError(400, "Avatar file is required");
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
@@ -21,4 +22,4 @@ const uploadOnCloudinary = async (localFilePath) => {
   }
 };
 
-export { cloudinary };
+export { uploadOnCloudinary };
